0dc59c790b29ca7ec9d4b6b53bf47f3f647f7d8d
Amrita Yadav <yadamrita936@gmail.com>
2025-12-21 20:28:52 +0530

    Time: 1923 ms (5.02%), Space: 55.9 MB (9.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,20 @@
-public class Solution {
-    public int majorityElement(int[] num) {
-
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
-                count++;
-                major=num[i];
-            }else if(major==num[i]){
-                count++;
-            }else count--;
-            
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            // Count occurrences of nums[i]
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                    count++;
+                }
+            }
+            // If count exceed n/2, nums[i] is the majority element
+            if (count > n / 2) {
+                return nums[i];
+            }
         }
-        return major;
+        return -1; // Should never be reached if majority element assumption holds
     }
 }
     
