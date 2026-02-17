905b5982cea785914e52ade9e98575f28a999198
Amrita Yadav <yadamrita936@gmail.com>
2026-02-17 19:14:32 +0530

    Time: 23 ms (81.24%), Space: 81.5 MB (55.11%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Loop through each element in the array as the first element of the pair
-        for (int i = 0; i < nums.length; i++) {
-            // Loop through each element after the i-th element
-            for (int j = i + 1; j <= i + k && j < nums.length; j++) {
-                // If the same element is found within k distance, return true
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
-            }
+     Set<Integer>map=new HashSet<>(k);
+     for(int i=0;i<nums.length;i++){
+         if(i>k){
+    map.remove(nums[i-k-1]);
         }
-        // If no such pair is found, return false
-        return false;
-    }
+
+        if(!map.add(nums[i])){
+            return true;
+        }
+       
+     }
+
+return false;
+
 }
+  
+    }
