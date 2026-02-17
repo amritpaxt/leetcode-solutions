677aefde140cc3676dde2f917bb4349687cc8672
Amrita Yadav <yadamrita936@gmail.com>
2026-02-17 19:15:43 +0530

    Time: 5 ms (30.41%), Space: 44.5 MB (34.94%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -0,0 +1,19 @@
+public class Codec {
+    private HashMap<Integer, String> map = new HashMap<>();
+    private int id = 0;
+    private static final String BASE_URL = "http://tinyurl.com/";
+
+    public String encode(String longUrl) {
+        // Store the long URL with current id
+        map.put(id, longUrl);
+        // Create a tiny URL by appending id to base URL and return it
+        return BASE_URL + id++;
+    }
+
+    public String decode(String shortUrl) {
+        // Extract the id from the short URL
+        int id = Integer.parseInt(shortUrl.replace(BASE_URL, ""));
+        // Get the long URL using the id from the map
+        return map.get(id);
+    }
+}
